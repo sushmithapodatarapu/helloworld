fb86ceb1fe1f2d5f0468670f5c39720b530bdd0c
sushmithapodatarapu <podatarapusushmitha7069@gmail.com>
2024-03-12 11:53:34 +0530

    hello

---
+++ src/gitpractice/HelloWorld.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/gitpractice/helloword.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class helloword
+{
+	public static void main(String[] args)
+	{
+		
+	System.out.println("Hello rbg");
+	}
+}	
