285efbd1789e31d1f2be6820240ee51ed383ade1
sushmithapodatarapu <podatarapusushmitha7069@gmail.com>
2024-03-15 11:15:52 +0530

    hiii

--- src/gitpractice/hellorbg.java
+++ src/gitpractice/hellorbg.java
@@ -1,9 +1,9 @@
 package gitpractice;
 
-public class HelloWorld {
+public class hellorbg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 System.out.println("hii");
 
 	}
 
